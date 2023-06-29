67fbf5116bac6d15c6fe270bf3ff6d6520f714a1
EugeBr <eugebordenave@gmail.com>
2023-06-29 16:49:33 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {UsuarioComponent} from './usuario.component';
+import { AppRoutingModule } from './app-routing.module';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsuarioComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/usuario.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-usuario',
+    templateUrl: './usuario.component.html'
+})
+
+export class UsuarioComponent{
+    usuarioNombre = "Maria Eugenia";
+
+}
