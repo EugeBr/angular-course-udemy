550b014942474b6e7999d796678e9d1d005f6aae
EugeBr <eugebordenave@gmail.com>
2023-07-03 12:37:52 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { InicioComponent } from './inicio/inicio.component';
+import { LibrosComponent } from './libros/libros.component';
 
-
+const routes: Routes = [
+  { path: '', component: InicioComponent },
+  { path: 'libros', component: LibrosComponent }
+]
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
